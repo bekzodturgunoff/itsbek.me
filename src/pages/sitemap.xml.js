13c4b9cfe0649ea7a958a53ea4c8e71406810cc1
Bekzod <bekzodturgunoff@gmail.com>
2026-05-12 22:28:02 +0500

    robotos fixed to make SEO better

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -1,7 +1,7 @@
 export const prerender = true;
 export const GET = async () => {
-  const base = 'https://example.com';
-  const urls = ['/', '/#about', '/#skills', '/#work', '/#contact'];
+  const base = 'https://itsbek.me';
+  const urls = ['/', '/ko/', '/ru/'];
   const body = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${urls.map(u => `<url><loc>${base}${u}</loc></url>`).join('\n')}\n</urlset>`;
   return new Response(body, { headers: { 'Content-Type': 'application/xml' } });
 };
